3a173c660b7c8b1c647c59d71607e04749997f7c
Maria Takei <g2220527is.ocha.ac.jp>
2024-11-15 13:13:34 +0900

    second commit

--- script.js
+++ script.js
@@ -40,22 +40,7 @@ function list_up_todo() {
     count += 1
 }
 
-function edit_todo() {
-    
-}
 
-fetch('http://localhost:8080/hello')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    }) 
-    .then(data => {
-        console.log(data.message);
-    })
-    .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-})
+
 
 
